acb811bf7bccb0f9dfed9d851ff3152d9cbd9d7d
Fernando Corrales <corralesfernando10@gmail.com>
2022-01-26 13:49:05 -0300

    Creando Plantilla Base

--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from ProyectoWebApp.views import home, services, blog, contact, marketplace
 
 urlpatterns = [
-    path('inicio', home),
-    path('servicios', services),
-    path('tienda', marketplace),
-    path('blog', blog),
-    path('contacto/', contact),
+    path('inicio', home, name="home"),
+    path('servicios', services, name="servicios"),
+    path('tienda', marketplace, name="tienda"),
+    path('blog', blog, name="blog"),
+    path('contacto/', contact, name="contacto"),
 ]
