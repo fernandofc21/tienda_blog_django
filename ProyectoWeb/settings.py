7134232d975e25d62c9021c6f23346413c22a5e9
Fernando Corrales <38081227+fernandofc21@users.noreply.github.com>
2022-03-14 13:31:17 -0300

    Update settings.py

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "fercovic10@gmail.com"
-EMAIL_HOST_PASSWORD = "Pilsen478*"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
